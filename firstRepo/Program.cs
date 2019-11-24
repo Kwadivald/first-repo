d13af715b11aa1bc3841415d8a29b5f5ea73eb8d
Mateusz Naumowicz <naumowicz@protonmail.com>
2019-11-24 07:33:03 +0100

    Revloution in app

--- firstRepo/Program.cs
+++ firstRepo/Program.cs
@@ -12,12 +12,14 @@ namespace firstRepo
         {
             while (true)
             {
+                int menuOption = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("1. Dodaj liczby");
-                Console.WriteLine("2. Odejmij liczby");
-                Console.WriteLine("3. Pomnóż liczby");
                 Console.WriteLine("4. Dziel liczby");
                 Console.WriteLine("5. Wyjście");
-                int menuOption = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("2. Odejmij liczby");
+                Console.WriteLine("3. Pomnóż liczby");
+                
+                int a = Convert.ToInt32(Console.ReadLine());
             }
         }
     }
