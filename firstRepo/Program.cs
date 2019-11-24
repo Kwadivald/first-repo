c29ce90dfce3abb4c67bdb3b470cbadc8576e603
Mateusz Naumowicz <naumowicz@protonmail.com>
2019-11-24 07:08:47 +0100

    Added loop to app

--- firstRepo/Program.cs
+++ firstRepo/Program.cs
@@ -10,11 +10,15 @@ namespace firstRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Dodaj liczby");
-            Console.WriteLine("2. Odejmij liczby");
-            Console.WriteLine("3. Pomnóż liczby");
-            Console.WriteLine("4. Dziel liczby");
-            Console.WriteLine("5. Wyjście");
+            while (true)
+            {
+                Console.WriteLine("1. Dodaj liczby");
+                Console.WriteLine("2. Odejmij liczby");
+                Console.WriteLine("3. Pomnóż liczby");
+                Console.WriteLine("4. Dziel liczby");
+                Console.WriteLine("5. Wyjście");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
