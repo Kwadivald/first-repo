33db24ae2e0ba4b1d7648b1b21331119f039e2db
Mateusz Naumowicz <naumowicz@protonmail.com>
2019-11-24 07:21:42 +0100

    Added exit option

--- firstRepo/Program.cs
+++ firstRepo/Program.cs
@@ -12,13 +12,17 @@ namespace firstRepo
         {
             while (true)
             {
-                int menuOption = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("1. Dodaj liczby");
-                Console.WriteLine("4. Dziel liczby");
-                Console.WriteLine("5. Wyjście");
                 Console.WriteLine("2. Odejmij liczby");
                 Console.WriteLine("3. Pomnóż liczby");
-                
+                Console.WriteLine("4. Dziel liczby");
+                Console.WriteLine("5. Wyjście");
+
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 5)
+                {
+                    break;
+                }
                 int a = Convert.ToInt32(Console.ReadLine());
             }
         }
