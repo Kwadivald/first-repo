1b74a4a938bd3d794418e73e846c4ab023499da0
Mateusz Naumowicz <naumowicz@protonmail.com>
2019-11-24 06:39:48 +0100

    Add menu of application

---
+++ firstRepo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Dodaj liczby");
+            Console.WriteLine("2. Odejmij liczby");
+            Console.WriteLine("3. Pomnóż liczby");
+            Console.WriteLine("4. Dziel liczby");
+            
+        }
+    }
+}
